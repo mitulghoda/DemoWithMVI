4862674dcc695017fdbd065f2511f14e7d23a74f
mitul.devstree <sikYsacALkawD8yUzyQq>
2023-08-22 18:49:47 +0530

    Add bottom menu with navigation graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.coroutine.core)
     implementation(libs.activity.ktx)
     implementation(libs.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 //    implementation(libs.androidx.baseLibrary)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
--- app/src/main/java/com/appearnings/baseapp/ui/MainActivity.kt
+++ app/src/main/java/com/appearnings/baseapp/ui/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.appearnings.baseapp
+package com.appearnings.baseapp.ui
 
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.appearnings.baseapp.MyEvent
+import com.appearnings.baseapp.MyViewModel
+import com.appearnings.baseapp.R
 import com.appearnings.baseapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.gson.Gson
 import kotlinx.coroutines.launch
 
@@ -17,8 +23,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         viewModel.state.handleEvent(MyEvent.Loading)
-        binding.btnClickMe.setOnClickListener {
-            viewModel.clickOnSingleData()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.bottomMenu)
+        navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
         }
     }
 
